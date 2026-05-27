7ad342506fefedc2d39c870d8d653db04d8cd359
Aris <blackleotasher@gmail.com>
2026-05-27 09:01:16 +0000

    almost done

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const temperature=36.6789
-console.log(temperature.toFixed(1))
-const a=17
-const b=5
-console.log(Math.floor(a/b))
-console.log(a%b)
-console.log(Math.floor(Math.random()*10)+1)
-console.log(isNaN("100"/5))
-console.log(isNaN("сто"/5))
-console.log(Math.max(14, -3, 77, 0, -100, 42))
-console.log(Math.min(14, -3, 77, 0, -100, 42))
-
-const sentence = "  javascript — это просто!  "
-console.log((sentence.at(2).toUpperCase())+((sentence.trim()).slice(1,23)))
-const role = "Студент"
-const last_name = "Картушин"
-const name = "Кирилл"
-const role_group = "группа"
-const group = "21-ИСР"
-const mid = "средний"
-const point = "балл:"
-const points = "4.2"
-console.log(`${role} ${last_name} ${name}, ${role_group} ${group}, ${mid} ${point} ${points}`)
-const date = "2025-03-15"
-const splitdate = (date.split("-")).reverse()
-console.log(`${splitdate[0]}.${splitdate[1]}.${splitdate[2]}`)
-const dev = "Node.js разработчик"
-console.log(dev.startsWith("Node"), dev.en)
+console.log("Привет, мир!")
+console.log(2 + 2)
+console.log(typeof "hello")
